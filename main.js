6243547f3027f763d4cb23cc98afaeefb46366d8
sayalikohad <138372300+sayalikohad@users.noreply.github.com>
2024-10-06 19:43:07 +0530

    main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+(function (jQuery){
+    "use strict";
+    jQuery(document).ready(function(){
+        function activaTav(pill){
+            jQuery(pill).addClass('active show');
+        }
+
+        // sticky header anmation and height 
+        function headerHeight(){
+            var height = jQuery("#main-header").height();
+            jQuery('.iq-height').css('height',height + 'px');
+        }
